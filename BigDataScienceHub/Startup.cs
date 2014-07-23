0a2b4a254b8f4cb574e52abe6a4c5ec6ab61e997
RajGopalakrishnan <rajlakshmipathi@gmail.com>
2014-07-22 17:50:11 -0700

    Upgraded to VS2013

---
+++ BigDataScienceHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigDataScienceHub.Startup))]
+namespace BigDataScienceHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
